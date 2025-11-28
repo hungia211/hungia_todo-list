fbbc56593adb817e4bb600dbf765080b50a2613a
Hữu Nghĩa <buihuunghia31082004@gmail.com>
2025-11-28 20:47:03 +0700

    final code

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,37 @@
-import express from 'express';
-import taskRoute from './routes/tasksRouters.js';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import taskRoute from "./routes/tasksRouters.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
 const app = express();
-
+const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173'}));
 
-app.use('/api/tasks', taskRoute);
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
+app.use("/api/tasks", taskRoute);
+
+// Phục vụ tệp tĩnh trong môi trường sản xuất
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Đang chạy cổng ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Đang chạy cổng ${PORT}`);
+  });
 });
-
